c364c850b3983ef6e920e8c7c5afc9df239be727
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-03-04 12:20:36 +0100

    Update Beauty.java

--- Prueba nivel/src/Beauty.java
+++ Prueba nivel/src/Beauty.java
@@ -3,10 +3,10 @@ class Beauty extends Product {
         private String vegane;
         private String season;
 
-    public Beauty (String name, String brand, double price, int stock, Date datepruchase, double discount,
+    public Beauty (String name, String brand, double price, int stock, Date datepurchase, double discount,
                    String vegane, String season) {
 
-        super (name, brand, price, stock, datepruchase, discount);
+        super (name, brand, price, stock, datepurchase, discount);
         this.vegane=vegane;
         this.season=season;
 
