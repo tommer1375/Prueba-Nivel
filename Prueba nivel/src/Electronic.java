898729ff77f1b52ac847d60674ef7c941c62f7e4
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-03-04 12:20:54 +0100

    Update Electronic.java

--- Prueba nivel/src/Electronic.java
+++ Prueba nivel/src/Electronic.java
@@ -5,9 +5,9 @@ class Electronic extends Product{
     private Integer battery;
     private static final int warranty=9;
 
-    public Electronic (String name, String brand, double price, int stock, Date datepruchase, double discount, int resolution,
+    public Electronic (String name, String brand, double price, int stock, Date datepurchase, double discount, int resolution,
                     int battery) {
-        super (name, brand, price, stock, datepruchase, discount);
+        super (name, brand, price, stock, datepurchase, discount);
         this.resolution=resolution;
         this.battery=battery;
     }
