2cc46c909563b0f73dee2436d8eaace92fbfa6d9
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-03-04 12:21:06 +0100

    Update Machine.java

--- Prueba nivel/src/Machine.java
+++ Prueba nivel/src/Machine.java
@@ -6,10 +6,10 @@ public class Machine extends Product{
     private Integer capacity;
     private static final int warranty=18;
 
-    public Machine (String name, String brand, double price, int stock, Date datepruchase, double discount, String energy,
+    public Machine (String name, String brand, double price, int stock, Date datepurchase, double discount, String energy,
                    Date dateStar, int capacity) {
 
-        super (name, brand, price, stock, datepruchase, discount);
+        super (name, brand, price, stock, datepurchase, discount);
         this.energy=energy;
         this.dateStar=dateStar;
         this.capacity=capacity;
